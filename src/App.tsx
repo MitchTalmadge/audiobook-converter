bbf2964954fea20db0ea84bfe1f9c2ef3e6ff354
Mitch Talmadge <mitch@talmad.ge>
2024-09-25 01:51:19 -0600

    Add wow

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Audiobook Tool</h1>
+        <h2>wow</h2>
+        <img src={require('./wow.png')} alt="doge" />
+        <h3>such empty</h3>
       </header>
     </div>
   );
